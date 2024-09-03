95e48b25164ea329a015e8f2b2003f8623262fb4
anishkasinha <anishkasinha@gmail.com>
2024-09-03 16:53:48 +0000

    solved issue

--- Sum2Num.java
+++ Sum2Num.java
@@ -1,5 +1,19 @@
 public class Sum2Num{
-  public static void Main(String[] args){
-    System.out.println("hi");
+  public static void main(String[] args){
+    int[] array = {10, 20, 30, 40, 50};
+    int target = 90;
+    int i = 0;
+    int j = array.length - 1;
+    while(i < j){
+      int totalNum = array[i] + array[j];
+      if(totalNum == target){
+        System.out.println(array[i] +"+"+array[j] + " adds to the target:" + target);
+         i++;
+      } else if (totalNum < target){
+       i++;
+      } else{
+       j--;
+      }
+    }    
   }
 }
